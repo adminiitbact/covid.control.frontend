7b4fb530fe7c49cfe29144c1189f7658e226bb58
Bhaskar <sharmabhaskar13@gmail.com>
2020-04-26 18:02:03 +0530

    facility add and list

--- src/modules/facility-add/index.js
+++ src/modules/facility-add/index.js
@@ -1,12 +1,21 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Header, Content } from 'components/layout';
 
 import FacilityAddForm from './facility-add-form';
+import FacilityAPI from 'api/facility';
 
-import { Button } from 'antd';
+import { Button, notification } from 'antd';
 
 export default function FacilityAdd(props) {
+  const [loading, setLoading] = useState(false);
   const formRef = useRef();
+  const reqRef = useRef();
+
+  useEffect(() => {
+    return () => {
+      reqRef.current && reqRef.current.abort();
+    };
+  }, []);
 
   const submitForm = () => {
     if (formRef.current) {
@@ -14,15 +23,48 @@ export default function FacilityAdd(props) {
     }
   };
 
+  const onSubmit = (values, actions) => {
+    setLoading(true);
+    reqRef.current && reqRef.current.abort();
+    const req = FacilityAPI.create({
+      facility: values
+    });
+    reqRef.current = req;
+    req
+      .then(res => {
+        notification.success({
+          message: 'Facility',
+          description: 'Facility has been added'
+        });
+        actions.setSubmitting(false);
+        setLoading(false);
+      })
+      .catch(err => {
+        notification.error({
+          message: 'Facility',
+          description: 'Something went wrong, please try again later'
+        });
+        actions.setSubmitting(false);
+        setLoading(false);
+      });
+  };
+
   return (
     <>
       <Header fixed>
         <div className='full-height d--f ai--c jc--fe'>
-          <Button size='large' onClick={submitForm} type='primary'>SUBMIT</Button>
+          <Button
+            loading={loading}
+            size='large'
+            onClick={submitForm}
+            type='primary'
+          >
+            SUBMIT
+          </Button>
         </div>
       </Header>
       <Content>
-        <FacilityAddForm innerRef={formRef} />
+        <FacilityAddForm innerRef={formRef} onSubmit={onSubmit} />
       </Content>
     </>
   );
