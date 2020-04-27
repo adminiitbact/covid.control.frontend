decf1a3452fd78dd16c3b0c3bb8ea897136b69fb
Bhaskar <sharmabhaskar13@gmail.com>
2020-04-28 00:54:31 +0530

    Revert "srcoll fix"
    This reverts commit f4f6cca711e41d516fc3d216be3a934995fcab97.

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,38 +1,16 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useRef, useEffect, useState } from 'react';
 import { Header, Content } from 'components/layout';
 import { Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
-import useResizeAware from 'react-resize-aware';
 
 import './home.scss';
 
 const antIcon = (
   <LoadingOutlined style={{ fontSize: 52, fontWeight: 500 }} spin />
 );
-
-const BIscreenRatio = 16 / 9;
-
-function getFrameHeightWidth(divHeight, divWidth) {
-  if (divWidth / divHeight > BIscreenRatio) {
-    return {
-      height: divHeight,
-      width: (4 / 3) * divHeight
-    };
-  } else {
-    return {
-      height: divHeight,
-      width: divWidth
-    };
-  }
-}
-
 export default function Home() {
   const iframeRef = useRef();
   const [loading, setloading] = useState(true);
-  const [iframeSize, setIframeSize] = useState();
-  const [resizeListener, sizes] = useResizeAware();
-
   function iframeLoadListener(e) {
     setloading(false);
   }
@@ -44,15 +22,11 @@ export default function Home() {
     return () => {
       iframeRef &&
         iframeRef.current &&
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         iframeRef.current.removeEventListener('load', iframeLoadListener);
     };
   }, []);
 
-  useEffect(() => {
-    setIframeSize(getFrameHeightWidth(sizes.height - 116, sizes.width));
-  }, [sizes.width, sizes.height]);
-
-  console.log(sizes, iframeSize);
   return (
     <>
       <Header>Dashboard</Header>
@@ -61,7 +35,6 @@ export default function Home() {
           padding: 0
         }}
       >
-        {resizeListener}
         <div className='iframe-container d--f ai--c jc--c'>
           {loading && (
             <div className='spin-container'>
@@ -72,12 +45,9 @@ export default function Home() {
             title='bi'
             src='https://datastudio.google.com/embed/reporting/b14ec87e-51df-4d9f-9290-4facc967cae8/page/HusNB'
             frameborder='0'
-            style={Object.assign(
-              {
-                border: 0
-              },
-              iframeSize
-            )}
+            style={{
+              border: 0
+            }}
             allowfullscreen
             ref={iframeRef}
           ></iframe>
