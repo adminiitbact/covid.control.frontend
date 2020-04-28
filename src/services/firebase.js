2871c60837a28c61d833cb7bf20a66851312e1ea
Bhaskar <sharmabhaskar13@gmail.com>
2020-04-29 03:55:55 +0530

    add firebase config in proper way

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,5 +2,4 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
-
 export const myFirebase = firebase.initializeApp(firebaseConfig);
