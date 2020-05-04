dd26d9c2b6602f32eeef3823e2f8263aa9d70feb
Bhaskar <sharmabhaskar13@gmail.com>
2020-05-05 00:27:41 +0530

    add power bi embed

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import { Header, Content } from 'components/layout';
 import { Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
+// import { Report } from 'react-powerbi';
 
 import './home.scss';
 
@@ -41,14 +42,24 @@ export default function Home() {
               <Spin indicator={antIcon} />
             </div>
           )}
-          <iframe
-            title='bi'
+          {/* <iframe
+            title='google-data-studio-bi'
             src='https://datastudio.google.com/embed/reporting/b14ec87e-51df-4d9f-9290-4facc967cae8/page/HusNB'
             frameBorder='0'
             style={{
               border: 0
             }}
             allowFullScreen
+            ref={iframeRef}
+          ></iframe> */}
+          <iframe
+            title='power-bi'
+            src='https://app.powerbi.com/reportEmbed?reportId=c9eb8da5-f612-4e4b-bc44-d9c7054bf2f1&autoAuth=true&ctid=cb1627f7-fe0a-4843-af89-c51f3a99d8a9&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9'
+            frameborder='0'
+            style={{
+              border: 0
+            }}
+            allowFullScreen='true'
             ref={iframeRef}
           ></iframe>
         </div>
