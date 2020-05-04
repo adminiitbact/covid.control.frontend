7a56df62b162efe28313c1d826961bc41e1ffdf6
Bhaskar <sharmabhaskar13@gmail.com>
2020-05-04 23:49:36 +0530

    add bed avaibility

--- src/modules/facility-edit/facility-linking/linking-table.js
+++ src/modules/facility-edit/facility-linking/linking-table.js
@@ -1,52 +1,127 @@
 import React from 'react';
 import Table from 'components/table';
 
+import './linking-table.scss';
+
 import InfinitePagination from 'components/infinite-pagination';
+
+function AvailabilityStatus({ label, occupied, total, available }) {
+  return (
+    <div className='d--f availibility-status mt2'>
+      <span className='mr1'>{label}</span>
+      <div className='d--f fw--n mr1 bold'>
+        <span>{occupied}</span>/<span>{total}</span>
+      </div>
+      <div className='green'>({available})</div>
+    </div>
+  );
+}
 
 const columns = [
   {
     dataIndex: 'name',
     title: 'Name of the Facility',
-    width: '30%'
+    width: '30%',
+    render: text => (
+      <div>
+        {text}
+        <div className='mt2 placeholder' />
+      </div>
+    )
   },
   {
     key: 'covidFacilityType',
-    render: (text, record, index) => (
+    dataIndex: 'covidFacilityType',
+    render: text => (
       <div>
-        <div>{record.covidFacilityType}</div>
-        <div>{record.availabilityStatusList}</div>
+        {text}
+        <div className='mt2 placeholder' />
       </div>
     )
   },
   {
     key: 'facilityStatus',
     title: 'Status',
-    render: (text, record, index) => (
-      <div>
-        <div>{record.Status}</div>
-        <div>{record.availabilityStatusList}</div>
-      </div>
-    )
+    dataIndex: 'facilityStatus',
+    render: (text, record, index) => {
+      // return null;
+      const mildObj = record.availabilityStatusList.find(
+        el => el.severity === 'MILD'
+      );
+      let statusCom = <div className='mt2 placeholder' />;
+      if (mildObj) {
+        statusCom = (
+          <AvailabilityStatus
+            label='Mild'
+            occupied={mildObj.totalBeds - mildObj.availableBeds}
+            total={mildObj.totalBeds}
+            available={mildObj.availableBeds}
+          />
+        );
+      }
+      return (
+        <div className='d--f fd--c jc--sb'>
+          <div>{text}</div>
+          {statusCom}
+        </div>
+      );
+    }
   },
   {
     key: 'jurisdiction',
     title: 'Jurisdiction',
-    render: (text, record, index) => (
-      <div>
-        <div>{record.jurisdiction}</div>
-        <div>{record.jurisdiction}</div>
-      </div>
-    )
+    dataIndex: 'jurisdiction',
+    render: (text, record, index) => {
+      // return null;
+      const mildObj = record.availabilityStatusList.find(
+        el => el.severity === 'MODERATE'
+      );
+      let statusCom = <div className='mt2 placeholder' />;
+      if (mildObj) {
+        statusCom = (
+          <AvailabilityStatus
+            label='Moderate'
+            occupied={mildObj.totalBeds - mildObj.availableBeds}
+            total={mildObj.totalBeds}
+            available={mildObj.availableBeds}
+          />
+        );
+      }
+      return (
+        <div className='d--f fd--c jc--sb'>
+          <div>{text}</div>
+          {statusCom}
+        </div>
+      );
+    }
   },
   {
     key: 'area',
     title: 'Area',
-    render: (text, record, index) => (
-      <div>
-        <div>{record.Status}</div>
-        <div>{record.area}</div>
-      </div>
-    )
+    dataIndex: 'area',
+    render: (text, record, index) => {
+      // return null;
+      const mildObj = record.availabilityStatusList.find(
+        el => el.severity === 'SEVERE'
+      );
+      let statusCom = <div className='mt2 placeholder' />;
+      if (mildObj) {
+        statusCom = (
+          <AvailabilityStatus
+            label='Severe'
+            occupied={mildObj.totalBeds - mildObj.availableBeds}
+            total={mildObj.totalBeds}
+            available={mildObj.availableBeds}
+          />
+        );
+      }
+      return (
+        <div className='d--f fd--c jc--sb'>
+          <div>{text}</div>
+          {statusCom}
+        </div>
+      );
+    }
   }
 ];
 
